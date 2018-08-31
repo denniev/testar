126ebd1a379fff206787564ba4e9aae18ba49eed
Dennie Vilumsons <dennie@brainworks.se>
2018-08-31 15:11:49 +0200

    First commit

---
+++ src/DennieServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Brainworks\Dennie;
+
+use Illuminate\Support\ServiceProvider;
+
+class DennieServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
